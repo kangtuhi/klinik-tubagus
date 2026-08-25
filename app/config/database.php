e70af080873a90ceb1fe20159742f3fbf01bb4b7
Kang Tuhi <tuhialkhan@gmail.com>
2026-08-25 23:49:59 +0700

    feat: add database configuration

---
+++ app/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => getenv('DB_HOST') ?: '127.0.0.1',
+    'port' => getenv('DB_PORT') ?: '3306',
+    'name' => getenv('DB_NAME') ?: 'klinik_tubagus',
+    'user' => getenv('DB_USER') ?: 'root',
+    'pass' => getenv('DB_PASS') ?: '',
+    'charset' => 'utf8mb4',
+];
